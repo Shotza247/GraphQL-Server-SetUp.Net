8fd6513ecd23957d5fe7066d4d76c74d6ea072db
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-09-29 14:22:42 +0200

    Testing if the server is setup correctly and the HotChocolate environment is accessible.

--- GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
+++ GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fc687971213d1936f8469624c30a4f573fba95")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
