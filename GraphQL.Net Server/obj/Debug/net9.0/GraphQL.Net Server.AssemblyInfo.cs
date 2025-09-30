bb598a309e10d6bd81547e82dc5a021588ec682f
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-09-30 12:14:31 +0200

    Added MongoDB.Driver → Official MongoDB C# driver
    HotChocolate → Popular GraphQL server for .NET
    
    HotChocolate.Data → Helps with filtering, sorting, projections

--- GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
+++ GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fc687971213d1936f8469624c30a4f573fba95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3074de086f29ba06b5168a50f7e0caf8dc3243c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
