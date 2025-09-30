f2efd92f419db4d44c836dc7842bd26b166d91e4
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-09-30 20:04:02 +0200

    We successfully created our schema within our server, however mongodb creates collections once documents exist inside them,  this means our connection has been established but no schema exists on the mongodb cluster. Thats whats nex.

--- GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
+++ GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e57b929eb4522121c5b737811487ccea12ff519")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70d61d5fb6a0a6a94f7ebea1a341494ca957109")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
