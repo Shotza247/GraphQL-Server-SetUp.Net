d45587c9cc7f4bf7a51410810d5a6a5e19f8fc30
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-09-30 19:29:14 +0200

    Added MongoDb drivers and packages, added in my connection string, setup the mongodb client, and setup a try catch test to confirm the DB Connection.

--- GraphQL.Net Server/Program.cs
+++ GraphQL.Net Server/Program.cs
@@ -1,4 +1,38 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using HotChocolate.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
+const string connectionUri = "mongodb+srv://jabulanindlovu360_db_user:xVcjEhfrZ3Rf2Kye@sbsa-test.wmi9dqy.mongodb.net/?retryWrites=true&w=majority&appName=SBSA-Test";
+
+// Setup MongoDB client
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+var client = new MongoClient(settings);
+
+try
+{
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Successfully connected to MongoDB!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"MongoDB connection failed: {ex}");
+}
+
+// Register MongoClient + Database in DI
+builder.Services.AddSingleton<IMongoClient>(client);
+builder.Services.AddSingleton(sp =>
+{
+    var mongoClient = sp.GetRequiredService<IMongoClient>();
+    return mongoClient.GetDatabase("SBSA-Test"); // 
+});
+
+
 builder.Services.AddGraphQLServer();
 var app = builder.Build();
 
