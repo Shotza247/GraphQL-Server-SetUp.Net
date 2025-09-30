603c0dc4c1a3978cccae353441de94830bbf9f6a
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-09-30 19:29:55 +0200

    Project ran successfully and mongodb was connected successfully.

--- GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
+++ GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3074de086f29ba06b5168a50f7e0caf8dc3243c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e57b929eb4522121c5b737811487ccea12ff519")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
