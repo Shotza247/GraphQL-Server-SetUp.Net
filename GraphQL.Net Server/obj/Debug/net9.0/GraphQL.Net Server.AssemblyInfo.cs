339d1da970b99759ea905ef944ef732faa8e14e3
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-10-01 10:07:44 +0200

    File generated and modified with every execution and build of the server.

--- GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
+++ GraphQL.Net Server/obj/Debug/net9.0/GraphQL.Net Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b70d61d5fb6a0a6a94f7ebea1a341494ca957109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e17485a18124feeeab8dc9089cf3745123dbbf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphQL.Net Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
