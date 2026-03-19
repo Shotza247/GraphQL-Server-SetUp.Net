cb9a59c85eb18d6fe567c4f9d9512785594fd93f
Jabulani Ndlovu <140488049+Shotza247@users.noreply.github.com>
2026-03-19 10:29:17 +0200

    Update AddEmployeeInput.cs
    AddEmployeeInput declares CurrentRole, DesiredRole, and Skills using the domain model types Role and SkillProficiency, which carry MongoDB BSON attributes like [BsonId]. HotChocolate will attempt to expose those as GraphQL input types, which can produce schema generation errors and causes the dedicated RoleInput / SkillProficiencyInput classes to be completely bypassed.

--- GraphQL.Net Server/Models/AddEmployeeInput.cs
+++ GraphQL.Net Server/Models/AddEmployeeInput.cs
@@ -1,8 +1,12 @@
 public class AddEmployeeInput
 {
     public string? Name { get; set; }
-    public Role? CurrentRole { get; set; }
-    public List<SkillProficiency>? Skills { get; set; } = new();
-    public Role? DesiredRole { get; set; }
+    
+    public RoleInput? CurrentRole { get; set; }
+
+    public List<SkillProficiencyInput>? Skills { get; set; } = new();
+
+    public RoleInput? DesiredRole { get; set; }
+
     public bool? IsMentor { get; set; }
 }
